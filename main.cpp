e936d2590413204de25997ba59054605091c6bdb
Vjameli <vjameli@gmail.com>
2025-02-26 12:13:02 -0500

    refactor 1.2

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ void initializeTargetPoints(int nbPoints, std::vector<double> &Xtarget,
   }
 }
 
-void sample_ellipse_border(int nbPoints, std::vector<double> &Xtarget,
+void sampleEllipseBorder(int nbPoints, std::vector<double> &Xtarget,
                            std::vector<double> &Ytarget,
                            std::vector<double> &PolarR,
                            std::vector<double> &PolarTheta) {
@@ -63,7 +63,7 @@ void sample_ellipse_border(int nbPoints, std::vector<double> &Xtarget,
                         PolarTheta);
 }
 
-void sample_ellipse_inside(int nbPoints, std::vector<double> &Xtarget,
+void sampleEllipseInside(int nbPoints, std::vector<double> &Xtarget,
                            std::vector<double> &Ytarget,
                            std::vector<double> &PolarR,
                            std::vector<double> &PolarTheta,
@@ -77,6 +77,33 @@ void sample_ellipse_inside(int nbPoints, std::vector<double> &Xtarget,
   Xt_in.insert(Xt_in.end(), Xtarget.begin(), Xtarget.end());
 }
 
+void computeCoilCenters(vector<double> &NPZ, vector<double> &NPR,
+                             vector<double> &SPZ, vector<double> &SPR,
+                             double zmin, double zmax, double rmin, double rmax,
+                             double step) {
+  for (int zc0 = zmin; zc0 <= zmax;) {
+    for (int rc0 = rmin; rc0 <= rmax;) {
+      NPZ.push_back(zc0 / 100.0);
+      NPR.push_back(rc0 / 100.0);
+      SPZ.push_back(-zc0 / 100.0);
+      SPR.push_back(rc0 / 100.0);
+      rc0 = rc0 + step;
+    }
+    zc0 = zc0 + step;
+  }
+}
+
+void computeCrossSections(vector<double> &cross_sections,
+                               vector<double> &widths, double Gamma) {
+
+  for (int value = 50; value <= 100; value += 5) {
+    widths.push_back(value / 1000.0);
+  }
+
+  for (unsigned int i = 0; i < widths.size(); i++) {
+    cross_sections.push_back(widths[i] * widths[i] / Gamma);
+  }
+}
 void computeAMNS(vector<vector<vector<double>>> &AMNS, int Nt, int Nc, int Ns,
                  vector<double> &Zt, vector<double> &Xt, vector<double> &CoilsZ,
                  vector<double> &CoilsR, vector<double> &cross_sections,
@@ -108,7 +135,7 @@ void computeAMNS(vector<vector<vector<double>>> &AMNS, int Nt, int Nc, int Ns,
   }
 }
 
-void AMNS_to_excel(const vector<vector<vector<double>>> &AMNS, int Nt, int Nc,
+void AMNSToExcel(const vector<vector<vector<double>>> &AMNS, int Nt, int Nc,
                    int Ns) {
   ofstream ExcelFile3;
   ExcelFile3.open("AMNS.csv");
@@ -124,7 +151,16 @@ void AMNS_to_excel(const vector<vector<vector<double>>> &AMNS, int Nt, int Nc,
   ExcelFile3.close();
 }
 
-void evaluate_solution_border(const vector<vector<double>> &SOL,
+void initialize2DSolVector(vector<vector<double>> &SOL, int Nc, int Ns) {
+  SOL.resize(Nc);
+  for (int n = 0; n < Nc; n++) {
+    SOL[n].resize(Ns);
+    for (int s = 0; s < Ns; s++) {
+      SOL[n][s] = 0;
+    }
+  }
+}
+void evaluateSolutionBorder(const vector<vector<double>> &SOL,
                               const vector<vector<vector<double>>> &AMNS,
                               int Nt, int Nc, int Ns, double *B) {
   // Initialize B array with zeros
@@ -146,7 +182,7 @@ void evaluate_solution_border(const vector<vector<double>> &SOL,
   }
 }
 
-void evaluate_solution_inside(const vector<vector<double>> &SOL, int Nt_in,
+void evaluateSolutionInside(const vector<vector<double>> &SOL, int Nt_in,
                               int Nc, int Ns, const vector<double> &Xt_in,
                               const vector<double> &Zt_in,
                               const vector<double> &CoilsR,
@@ -176,7 +212,7 @@ void evaluate_solution_inside(const vector<vector<double>> &SOL, int Nt_in,
   }
 }
 
-double compute_Peak2peak(const double *B, int Nt, double B_zero) {
+double computePeak2Peak(const double *B, int Nt, double B_zero) {
   double min_B = 10;
   double max_B = -10;
   for (int m = 0; m < Nt; m++) {
@@ -188,7 +224,7 @@ double compute_Peak2peak(const double *B, int Nt, double B_zero) {
   return 1e6 * ((max_B - B_zero) + (B_zero - min_B));
 }
 
-double compute_RMS(const double *B, int Nt) {
+double computeRMS(const double *B, int Nt) {
   double sum_square = 0;
   for (int m = 0; m < Nt; m++) {
     sum_square += B[m] * B[m];
@@ -196,7 +232,7 @@ double compute_RMS(const double *B, int Nt) {
   return sqrt(sum_square / Nt);
 }
 
-void write_outFile(const string &filename, double objValue, double duration,
+void writeOutfile(const string &filename, double objValue, double duration,
                    double Peak2Peak, double RMS, double Peak2Peak_in,
                    double RMS_in, const vector<vector<double>> &SOL,
                    const vector<double> &CoilsR, const vector<double> &CoilsZ,
@@ -224,7 +260,7 @@ void write_outFile(const string &filename, double objValue, double duration,
   outFile.close();
 }
 
-void print_outFile(const vector<vector<double>> &SOL, int Nc, int Ns,
+void printOutfile(const vector<vector<double>> &SOL, int Nc, int Ns,
                    const vector<double> &CoilsR, const vector<double> &CoilsZ,
                    const vector<double> &widths, double Gamma, double objValue,
                    double duration) {
@@ -241,7 +277,7 @@ void print_outFile(const vector<vector<double>> &SOL, int Nc, int Ns,
   }
 }
 
-void compute_magnetic_field_matrix(const vector<vector<double>> &SOL, int Nc,
+void computeMagneticFieldMatrix(const vector<vector<double>> &SOL, int Nc,
                                    int Ns, const vector<double> &CoilsR,
                                    const vector<double> &CoilsZ,
                                    const vector<double> &widths, double Gamma,
@@ -284,7 +320,7 @@ void compute_magnetic_field_matrix(const vector<vector<double>> &SOL, int Nc,
   }
 }
 
-void matrix_to_excel(const double B_MATRIX[61][66], const string &filename) {
+void matrixToExcel(const double B_MATRIX[61][66], const string &filename) {
   ofstream ExcelFile;
   ExcelFile.open(filename);
   for (int i = 0; i < 61; i++) {
@@ -322,7 +358,7 @@ int main() {
   initializeTargetPoints(nbPoints, Xtarget, Ytarget, PolarR, PolarTheta);
 
   // Sample optimization points on the ellipse's border
-  sample_ellipse_border(nbPoints, Xtarget, Ytarget, PolarR, PolarTheta);
+  sampleEllipseBorder(nbPoints, Xtarget, Ytarget, PolarR, PolarTheta);
 
   vector<double> Zt;
   vector<double> Rt;
@@ -335,6 +371,7 @@ int main() {
   Zt = Ytarget;
   Rt = PolarR;
 
+  // Initialiize Xt with the absolute value of Xtarget
   for (int i = 0; i < nbPoints; i++) {
     Xt[i] = abs(Xtarget[i]);
   }
@@ -348,30 +385,20 @@ int main() {
   Zt_in.push_back(0);
   Xt_in.push_back(0);
 
-  sample_ellipse_inside(nbPoints, Xtarget, Ytarget, PolarR, PolarTheta, Zt_in,
+  sampleEllipseInside(nbPoints, Xtarget, Ytarget, PolarR, PolarTheta, Zt_in,
                         Xt_in);
 
   int Nt_in = Zt_in.size();
 
   // Coordinates of the cente of the coils based on the step (IN METERS!)
   vector<double> NPZ; // North pole Z
-  vector<double> NPR; // North pole R
+  vector<double> NPR;
   vector<double> SPZ; // South pole Z
-  vector<double> SPR; // South pole R
+  vector<double> SPR;
 
-  for (int zc0 = zmin; zc0 <= zmax;) {
-    for (int rc0 = rmin; rc0 <= rmax;) {
-      NPZ.push_back(zc0 / 100.0);
-      NPR.push_back(rc0 / 100.0);
-      SPZ.push_back(-zc0 / 100.0);
-      SPR.push_back(rc0 / 100.0);
-      rc0 = rc0 + step;
-    }
-    zc0 = zc0 + step;
-  }
+  computeCoilCenters(NPZ, NPR, SPZ, SPR, zmin, zmax, rmin, rmax, step);
 
-  // CoilsZ = concatenation of (NPZ + SPZ)
-  // CoilsR = concatenation of (NPR + SPR)
+  // CoilsZ = concatenation of (NPZ + SPZ), analogous for CoilsR
   vector<double> CoilsZ;
   vector<double> CoilsR;
   CoilsZ = NPZ;
@@ -383,14 +410,9 @@ int main() {
   // Set of potential cross sections (Potential widths)
   // VALUE WAS ORIGINALLY EQUAL TO 5!!!!!!!!!! <-------
   vector<double> widths;
-  for (int value = 50; value <= 100; value += 5)
-    widths.push_back(value / 1000.0);
-
   vector<double> cross_sections;
 
-  for (unsigned int i = 0; i < widths.size(); i++) {
-    cross_sections.push_back(widths[i] * widths[i] / Gamma);
-  }
+  computeCrossSections(cross_sections, widths, Gamma);
 
   int Ns = cross_sections.size();
 
@@ -398,7 +420,8 @@ int main() {
   vector<vector<vector<double>>> AMNS;
   computeAMNS(AMNS, Nt, Nc, Ns, Zt, Xt, CoilsZ, CoilsR, cross_sections, widths,
               Gamma);
-  AMNS_to_excel(AMNS, Nt, Nc, Ns);
+
+  AMNSToExcel(AMNS, Nt, Nc, Ns);
 
   // Initialize vectors for optimization model and cpu time
   Model2 *design;
@@ -411,18 +434,12 @@ int main() {
 
   // Initialize 2D SOL vector
   vector<vector<double>> SOL;
-  SOL.resize(Nc);
-  for (int n = 0; n < Nc; n++) {
-    SOL[n].resize(Ns);
-    for (int s = 0; s < Ns; s++) {
-      SOL[n][s] = 0;
-    }
-  }
+
+  initialize2DSolVector(SOL, Nc, Ns);
 
   vector<vector<vector<double>>> SOL_VECTOR;
 
-  // Here, I removed the while loop for now.
-  cout << "************** NBCOILS = " << NbCoils << " **************" << endl;
+  // Optimization environment starts
   duration = 0;
   ChronoCPU chrono;
   chrono.start();
@@ -453,35 +470,35 @@ int main() {
     cout << filename << "  \n";
 
     double B[Nt];
-    evaluate_solution_border(SOL, AMNS, Nt, Nc, Ns, B);
+    evaluateSolutionBorder(SOL, AMNS, Nt, Nc, Ns, B);
 
     // Compute the resultant magnetic field from the solution inside the FOV.
     double B_in[Nt_in];
-    evaluate_solution_inside(SOL, Nt_in, Nc, Ns, Xt_in, Zt_in, CoilsR, CoilsZ,
+    evaluateSolutionInside(SOL, Nt_in, Nc, Ns, Xt_in, Zt_in, CoilsR, CoilsZ,
                              widths, cross_sections, Gamma, B_in);
 
     // Peak to peak on the border and inside of the FOV
     // RMS on the border and inside of the FOV
-    double Peak2Peak_in = compute_Peak2peak(B_in, Nt_in, B_zero);
-    double RMS_in = compute_RMS(B_in, Nt_in);
-    double Peak2Peak = compute_Peak2peak(B, Nt, B_zero);
-    double RMS = compute_RMS(B, Nt);
+    double Peak2Peak_in = computePeak2Peak(B_in, Nt_in, B_zero);
+    double RMS_in = computeRMS(B_in, Nt_in);
+    double Peak2Peak = computePeak2Peak(B, Nt, B_zero);
+    double RMS = computeRMS(B, Nt);
 
-    write_outFile(filename, design->getObjValue(), duration, Peak2Peak, RMS,
+    writeOutfile(filename, design->getObjValue(), duration, Peak2Peak, RMS,
                   Peak2Peak_in, RMS_in, SOL, CoilsR, CoilsZ, widths, Gamma, Ns);
   }
 
-  print_outFile(SOL, Nc, Ns, CoilsR, CoilsZ, widths, Gamma,
+  printOutfile(SOL, Nc, Ns, CoilsR, CoilsZ, widths, Gamma,
                 design->getObjValue(), duration);
 
   // Compute the magnetic field matrix
   double B_MATRIX[61][66];
-  compute_magnetic_field_matrix(SOL, Nc, Ns, CoilsR, CoilsZ, widths, Gamma,
+  computeMagneticFieldMatrix(SOL, Nc, Ns, CoilsR, CoilsZ, widths, Gamma,
                                 cross_sections, 0.0, -0.3, 0.65, 0.3, 0.01,
                                 B_MATRIX);
 
   // Write the magnetic field matrix to a CSV file
-  matrix_to_excel(B_MATRIX, "FieldTotal_Matrix.csv");
+  matrixToExcel(B_MATRIX, "FieldTotal_Matrix.csv");
 
   cout << "CODE IS DONE!!" << endl;
   return 0;
